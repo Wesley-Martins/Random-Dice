8de197fd2844fdff0bdd5aaaf2bb9019265119d6
Wésley P.Martins <weslley2013.pinto@gmail.com>
2023-02-25 19:46:00 -0300

    Terminando as principais features js do projeto.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 const themeBtn = document.querySelector(".theme__label");
+const rollBtn = document.getElementById("roll-btn");
 const optionsForm = document.querySelector(".options-form");
 const diceAmountInput = document.querySelector(".options-form__number");
 const diceTypeBtns = document.querySelectorAll("[data-type-btn]");
 const dicesContainer = document.querySelector(".dices__container");
-const dices = document.querySelectorAll(".dices");
 
-themeBtn.addEventListener("click", function() {
-    document.querySelector("body").classList.toggle("dark-theme")
-})
+function existingDices() {return document.querySelectorAll(".dices");}
+
+function randomInteger(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shakeDice(dice) {
+    dice.classList.remove("shake");
+    void dice.offsetWidth;
+    dice.classList.add("shake");
+}
 
 const diceList = [];
 function createDice(number) {
@@ -20,7 +28,7 @@ function createDice(number) {
             document.querySelector(".dices").remove();
         }
 
-        diceList.splice(1, amountToDelete)
+        diceList.splice(1, amountToDelete);
     }
     
     else {
@@ -37,10 +45,34 @@ function createDice(number) {
 }
 createDice(1);
 
+function changeDiceType() {
+    const dices = document.querySelectorAll(".dices");
+
+    dices.forEach(dice => {
+        dice.style.backgroundImage = `url(./img/dice-types/d${lastClickedBtn}.png)`;
+        dice.innerHTML = 1;
+    })
+}
+
 let lastClickedBtn;
 diceTypeBtns.forEach(button => {
-    button.addEventListener('click', (button) => {
-        console.log(button.target.getAttribute('data-type-btn'))
+    button.addEventListener("click", (event) => {
+        lastClickedBtn = event.target.getAttribute('data-type-btn');
+
+        changeDiceType()
+        
+    })
+})
+
+rollBtn.addEventListener("click", () => {
+    const dices = existingDices();
+
+    dices.forEach(dice => {
+        if(lastClickedBtn) {dice.innerHTML = randomInteger(1, lastClickedBtn)}
+    
+        else {dice.innerHTML = randomInteger(1, 20)}
+
+        shakeDice(dice);
     })
 })
 
@@ -56,5 +88,10 @@ diceAmountInput.addEventListener("input", function() {
     else {
         errorBox.style.display = "none";
         createDice(number);   
+        if(lastClickedBtn) {changeDiceType()};
     }
+})
+
+themeBtn.addEventListener("click", function() {
+    document.querySelector("body").classList.toggle("dark-theme")
 })
